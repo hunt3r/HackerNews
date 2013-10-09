52a0ce7cfc2ad022d3daf1fc2f0924a3dbe1ae8a
Chris Hunter <hunter.christopher@gmail.com>
2013-10-09 09:13:07 -0400

    First commit

---
+++ HackerNews/ArticlesViewController.h
@@ -0,0 +1,13 @@
+//
+//  ArticlesViewController.h
+//  HackerNews
+//
+//  Created by Chris Hunter on 10/8/13.
+//  Copyright (c) 2013 MotoProg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ArticlesViewController : UIViewController
+
+@end
---
+++ HackerNews/ArticlesViewController.m
@@ -0,0 +1,40 @@
+//
+//  ArticlesViewController.m
+//  HackerNews
+//
+//  Created by Chris Hunter on 10/8/13.
+//  Copyright (c) 2013 MotoProg. All rights reserved.
+//
+
+#import "ArticlesViewController.h"
+
+@interface ArticlesViewController : UIViewController <UITableViewDataSource, UITableViewDelegate> {
+    UITableView *firstTableView;
+}
+
+@end
+
+@implementation ArticlesViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
